e9a7d176b18dea7b4ea770d811c4ec88a52a4854
Humida <huynhminhdanh1110@gmail.com>
2021-05-04 16:13:15 +0700

    complete todo-list basic

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,49 @@ import './App.css';
 import TodoList from './components/TodoList';
 import checkAll from './components/icon/check-all.svg'
 
-
-
-
 class App extends Component {
   constructor(props){
     super(props);
-    this.todoData=[{act:'thuc day'},{act:'di ngu'},{act:'di an'}];
-    this.isActive=false;
-    
+    this.state= 
+      {todoItem:[
+        {act:'Thức dậy',isActive:false},
+        {act:'Đi ngủ',isActive:false},
+        {act:'Đi ăn',isActive:false}
+    ]};
+    this.addTodo=this.addTodo.bind(this);
+  } 
+  addTodo(e){  
+    if(e.keyCode===13){
+      let contentTodo=e.target.value.trim();
+      if(contentTodo){
+        this.setState(
+          {
+            todoItem:[
+              ...this.state.todoItem,{act:contentTodo,isActive:false}
+            ]
+          }
+        )
+      }
+      e.target.value='';
+    }
+    else{
+      return;
+    }
   }
- 
   render() {
     return(
       <div className="App">
         <div className="addTodo-item">
           <img src={checkAll} alt="" height={32} width={32}/>
-          <input type="text"/>
+          <input type="text" onKeyDown={this.addTodo}/>
         </div>
+      <div className="app-container">
       {
-        this.todoData.map((item)=><TodoList keys={item.toString()} title={item.act}/>)
+        this.state.todoItem.map((item)=><TodoList keys={item.toString()} title={item.act}/>)
       }
       </div>
+    </div>
     )
   }
 }
-
 export default App;
